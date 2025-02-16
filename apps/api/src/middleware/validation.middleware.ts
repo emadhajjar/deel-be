43fb7811b1146a39d4e710b14fc86bc4ebaf5ce6
Emad Hajjar <emadaldeen.hajjar@gmail.com>
2025-02-16 06:12:02 -0300

    chore: add validation

---
+++ apps/api/src/middleware/validation.middleware.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { z, ZodError } from 'zod';
+
+export function validateData(schema: z.ZodObject<any, any>, query = false) {
+  return (request: Request, response: Response, next: NextFunction) => {
+    try {
+      schema.parse(query ? request.query : request.body);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errorMessages = error.errors.map((issue: any) => ({
+          message: `${issue.path.join('.')} is ${issue.message}`,
+        }));
+
+        response
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ details: errorMessages, error: 'Invalid data' });
+      } else {
+        response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
+      }
+    }
+  };
+}
